e5986ca47b4023140d47e48b0da4f965adef5c0e
Dion <dionvanderlinden@live.nl>
2019-09-24 15:42:21 +0200

    Added image url array to database

--- safari_map/lib/data/heatspot.dart
+++ safari_map/lib/data/heatspot.dart
@@ -7,13 +7,15 @@ class Heatspot {
   Timestamp _time;
   String _description;
   DroneType _drone;
+  List<String> _images;
 
-  Heatspot(String id, GeoPoint location, Timestamp time, String description, DroneType drone) {
+  Heatspot(String id, GeoPoint location, Timestamp time, String description, DroneType drone, List<String> images) {
     this._id = id;
     this._location = location;
     this._time = _time;
     this._description = description;
     this._drone = drone;
+    this._images = images;
   }
 
   String get description => _description;
@@ -26,11 +28,14 @@ class Heatspot {
 
   DroneType get drone => _drone;
 
+  List<String> get images => _images;
+
   static Heatspot getHeatspot(DocumentSnapshot doc) {
     var data = doc.data;
     // TODO Exception if key value does not exist
+    List<dynamic> images = data["images"];
     Heatspot hs = new Heatspot(doc.documentID, data["location"], data["time"],
-        data["description"], DroneType.values[data["droneID"]]);
+        data["description"], DroneType.values[data["droneType"]], images.cast<String>());
 
     return hs;
   }
